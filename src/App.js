e5121e4a9e96aab0cfc48840a24e0e840704007c
Alan Araujo <alan.santos@lasa.com.br>
2016-12-15 15:05:17 -0200

    add css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,87 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './css/pure-min.css';
+import './css/side-menu.css';
 
 class App extends Component {
+
+  
+  constructor(){
+    super();
+
+    this.state = {lista : [{nome:'joao barbosa', email:'joao.barbosa@caelum.com.br',senha:'123456'}]};
+  }
+
+
+  componentWillMount(){
+    console.log('componentWillMount');
+  }
+
+
   render() {
+
+    console.log('render');
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+<div id="layout">
+    
+    <a href="#menu" id="menuLink" className="menu-link">
+        <span></span>
+    </a>
+
+    <div id="menu">
+        <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Company</a>
+
+            <ul className="pure-menu-list">
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+            </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    </div>
+
+
+
+    <div id="main">
+        <div className="header">
+            <h1>Page Title</h1>
+            <h2>A subtitle for your page goes here</h2>
+        </div>
+
+        <div className="content">
+
+
+           <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {
+                    this.state.lista.map(function(autor){
+                      return(
+                        <tr>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                        )
+                      })
+                  }
+                </tbody>
+               
+              </table>
+          </div>
+
+        </div>
+    </div>
+</div>
     );
   }
 }
