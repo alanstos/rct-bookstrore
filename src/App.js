afee1b45f311a024a8b9d5e2f59470d05e2f0aed
Alan Araujo <alan.santos@lasa.com.br>
2016-12-15 19:13:45 -0200

    listagem da api

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
 
   
   constructor(){
     super();
-
-    this.state = {lista : [{nome:'joao barbosa', email:'joao.barbosa@caelum.com.br',senha:'123456'}]};
+    this.state = {lista : []};
   }
 
 
   componentWillMount(){
     console.log('componentWillMount');
+    $.ajax({
+        url : 'http://localhost:3001/api/autores',
+        method : 'get',
+        dataType: 'jsonp',
+
+
+        success : function(result){
+          console.log('sucesso');
+          this.setState({lista: result}).bind(this);
+
+        }.bind(this),
+        error : function (result){
+          console.log('deu errroooo');
+          console.log(result);
+        }.bind(this)
+    });
+
+
+
+
   }
 
 
@@ -47,12 +67,36 @@ class App extends Component {
 
     <div id="main">
         <div className="header">
-            <h1>Page Title</h1>
-            <h2>A subtitle for your page goes here</h2>
+            <h1>Cadastro de autores</h1>
         </div>
 
         <div className="content">
 
+          <form className="pure-form pure-form-aligned">
+                  <fieldset>
+                      <div className="pure-control-group">
+                          <label htmlFor="name">Nome</label>
+                          <input id="name" type="text" placeholder="Nome"/>
+                      </div>
+
+                      <div className="pure-control-group">
+                          <label htmlFor="email">Email Address</label>
+                          <input id="email" type="email" placeholder="Email Address"/>
+                      </div>
+
+                      <div className="pure-control-group">
+                          <label htmlFor="password">Password</label>
+                          <input id="password" type="password" placeholder="Password"/>
+                      </div>                      
+              
+
+              
+                      <div className="pure-controls">
+
+                          <button type="submit" className="pure-button pure-button-primary">Submit</button>
+                      </div>
+                  </fieldset>
+              </form>
 
            <div>
               <table className="pure-table">
