838a8bd960d73ed90ef079c2e1e6f8965a5ef946
Alan Araujo <alan.santos@lasa.com.br>
2016-12-19 18:34:46 -0200

    submit e atualizando listagem

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ class App extends Component {
   
   constructor(){
     super();
+    console.log('constructor app - inicio');
     this.state = {lista : [],nome :'', email : '', senha: ''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+    console.log('constructor app - fim');
   }
 
 
-  componentWillMount(){
-    console.log('componentWillMount');
+  componentDidMount(){
+    console.log('componentDidMount');
     $.ajax({
         url : 'http://localhost:8000/api/autores',
         method : 'get',
@@ -41,28 +47,42 @@ class App extends Component {
 
     console.log('enviando form.....');
 
-    console.log(event.target);
-
-    console.log(this.nome);
+    var idAutor = new Date().getMilliseconds();
 
     $.ajax({
-        url:"http//localhost:8080/api/autores",
+        url:'http://localhost:8000/api/autores/grava',
         contentType: 'application/json', //enviados 
         dataType:'json', //resposta
-        type:'post',
+        type:'POST',
         data: JSON.stringify({
+          id : idAutor,
           nome:this.state.nome,
-          email:this.state.nome,
-          senha:this.state.nome}),
-        sucess: function(resposta){
-          console.log("enviado com sucesso");
-        },
+          email:this.state.email,
+          senha:this.state.senha}),
+        success: function(resposta){
+          console.log('enviado com sucesso');
+
+          this.setState({lista:resposta});
+
+        }.bind(this),
         error: function(resposta){
             console.log("erro");
+            console.log(resposta);
         }
 
     });
+  }
 
+  setNome(event){
+    this.setState({nome : event.target.value});
+  }
+
+  setEmail(event){
+    this.setState({email : event.target.value});
+  }
+
+  setSenha(event){
+    this.setState({senha : event.target.value});
   }
 
 
@@ -99,21 +119,21 @@ class App extends Component {
 
         <div className="content">
 
-          <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm}>
+          <form className="pure-form pure-form-aligned" method="POST" onSubmit={this.enviaForm}>
                   <fieldset>
                       <div className="pure-control-group">
                           <label htmlFor="name">Nome</label>
-                          <input id="name" type="text" placeholder="Nome" value={this.state.nome} />
+                          <input id="name" type="text" placeholder="Nome" value={this.state.nome} onChange={this.setNome} />
                       </div>
 
                       <div className="pure-control-group">
                           <label htmlFor="email">Email Address</label>
-                          <input id="email" type="email" placeholder="Email Address" value={this.state.email}/>
+                          <input id="email" type="email" placeholder="Email Address" value={this.state.email} onChange={this.setEmail} />
                       </div>
 
                       <div className="pure-control-group">
                           <label htmlFor="password">Password</label>
-                          <input id="password" type="password" placeholder="Password"  value={this.state.senha}/>
+                          <input id="password" type="password" placeholder="Password"  value={this.state.senha} onChange={this.setSenha} />
                       </div>                      
               
 
